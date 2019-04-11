9b0d5251a6e88b0c67ddd31809b47e63971878b5
Bgsuu <48554997+Bgsuu@users.noreply.github.com>
2019-04-11 19:15:24 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=8CtKWzZUMs3QFFsFvra0dQqLjMTwXR0fm5imXuPF";
         
         $("#loadImage").click(function(){
             $.ajax({
